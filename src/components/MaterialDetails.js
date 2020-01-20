9e7e7375b1b789e01ec824904ae1116ada14e9d5
Nicholas Barooah <barooahn@gmail.com>
2020-01-20 18:28:32 +0000

    finished adding all fields

--- src/components/MaterialDetails.js
+++ src/components/MaterialDetails.js
@@ -1,18 +1,37 @@
 import React from "react"
 import TextField from "@material-ui/core/TextField"
-import { withStyles } from '@material-ui/core/styles';
-import { Autocomplete } from '@material-ui/lab';
+import { withStyles } from "@material-ui/core/styles"
+import { Autocomplete } from "@material-ui/lab"
+import Typography from "@material-ui/core/Typography"
+import Slider from "@material-ui/core/Slider"
 
 const styles = {
   inputWrapper: {
     display: "flex",
     flexDirection: "column",
-    padding: 20
-  }
+    padding: 20,
+  },
 }
 
-function MaterialDetails({ classes, title, setTitle, description, setDescription, levels, levelValue, setLevelValue }) {
-
+function MaterialDetails({
+  classes,
+  title,
+  setTitle,
+  description,
+  setDescription,
+  objective,
+  setObjective,
+  levels,
+  levelValue,
+  setLevelValue,
+  pupilTasks,
+  pupilTaskValue,
+  setPupilTaskValue,
+  timePrep,
+  setTimePrep,
+  timeClass,
+  setTimeClass,
+}) {
   const changeTitle = e => {
     setTitle(e.target.value)
   }
@@ -21,35 +40,104 @@ function MaterialDetails({ classes, title, setTitle, description, setDescription
     setDescription(e.target.value)
   }
 
+  const changeObjective = e => {
+    setObjective(e.target.value)
+  }
+
+  const changeTimePrep = (e, newValue) => {
+    setTimePrep(newValue)
+  }
+
+  const changeTimeClass = (e, newValue) => {
+    setTimeClass(newValue)
+  }
+
   const inputs = [
     {
       type: "text",
       label: "Title",
       multi: true,
       value: title,
-      onChange: changeTitle
+      onChange: changeTitle,
     },
     {
       type: "text",
       label: "Description",
       multi: true,
       value: description,
-      onChange: changeDescription
+      onChange: changeDescription,
+    },
+    {
+      type: "text",
+      label: "Objective",
+      multi: true,
+      value: objective,
+      onChange: changeObjective,
+    },
+    {
+      type: "slider",
+      label:
+        "Time needed for preparation (slide bar or type - number of minutes)",
+      value: timePrep,
+      onChange: changeTimePrep,
+    },
+    {
+      type: "slider",
+      label: "Time needed in class (slide bar or type - number of minutes)",
+      value: timeClass,
+      onChange: changeTimeClass,
     },
   ]
 
   return (
     <div className={classes.inputWrapper}>
       {inputs.map(input => {
-        return (
-          <TextField
-            label={input.label}
-            value={input.value}
-            onChange={input.onChange}
-          />
-        )
+        if (input.type === "text") {
+          return (
+            <TextField
+              label={input.label}
+              value={input.value}
+              onChange={input.onChange}
+            />
+          )
+        } else if (input.type === "slider") {
+          console.log(`value of ${input.label}: ${input.value}`)
+          return (
+            <div>
+              <Typography gutterBottom>{input.label}</Typography>
+              <Slider
+                getAriaLabel={index =>
+                  index === 0 ? "Minimum Minutes" : "Maximum Minutes"
+                }
+                value={input.value}
+                valueLabelDisplay="on"
+                onChange={input.onChange}
+              />
+            </div>
+          )
+        } else {
+          return null
+        }
       })}
 
+      <Autocomplete
+        id="combo-box-demo"
+        multiple
+        value={pupilTaskValue}
+        onChange={setPupilTaskValue}
+        options={pupilTasks}
+        freeSolo={true}
+        getOptionLabel={option => option.title}
+        style={{ width: 300 }}
+        renderInput={params => (
+          <TextField
+            {...params}
+            label="Combo box"
+            variant="outlined"
+            fullWidth
+          />
+        )}
+      />
       <Autocomplete
         id="combo-box-demo"
         multiple
@@ -60,11 +148,16 @@ function MaterialDetails({ classes, title, setTitle, description, setDescription
         getOptionLabel={option => option.title}
         style={{ width: 300 }}
         renderInput={params => (
-          <TextField {...params} label="Combo box" variant="outlined" fullWidth />
+          <TextField
+            {...params}
+            label="Combo box"
+            variant="outlined"
+            fullWidth
+          />
         )}
       />
     </div>
   )
 }
 
-export default withStyles(styles)(MaterialDetails);
+export default withStyles(styles)(MaterialDetails)
--- src/components/MaterialStepper.js
+++ src/components/MaterialStepper.js
@@ -8,6 +8,7 @@ import Typography from "@material-ui/core/Typography"
 import MediaFiles from "./media-files"
 import MaterialDetails from "./MaterialDetails"
 import { saveData } from "../actions/materials-share-actions"
+import MaterialDetailsFull from "./MaterialDetailsFull"
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -27,39 +28,142 @@ function getSteps() {
 }
 
 const levels = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 }
+  { title: "The Shawshank Redemption", year: 1994 },
+  { title: "The Godfather", year: 1972 },
+  { title: "The Godfather: Part II", year: 1974 },
+  { title: "The Dark Knight", year: 2008 },
+]
+
+const pupilTasks = [
+  { title: "The Shawshank Redemption task", year: 1994 },
+  { title: "The Godfather task", year: 1972 },
+  { title: "The Godfather: Part II task", year: 1974 },
+  { title: "The Dark Knight task", year: 2008 },
+]
+
+const institute = [
+  { title: "The Shawshank Redemption institute", year: 1994 },
+  { title: "The Godfather institute", year: 1972 },
+  { title: "The Godfather: Part II institute", year: 1974 },
+  { title: "The Dark Knight institute", year: 2008 },
+]
+const languageFocus = [
+  { title: "The Shawshank Redemption languageFocus", year: 1994 },
+  { title: "The Godfather languageFocus", year: 1972 },
+  { title: "The Godfather: Part II languageFocus", year: 1974 },
+  { title: "The Dark Knight languageFocus", year: 2008 },
+]
+const activityUse = [
+  { title: "The Shawshank Redemption activityUse", year: 1994 },
+  { title: "The Godfather activityUse", year: 1972 },
+  { title: "The Godfather: Part II activityUse", year: 1974 },
+  { title: "The Dark Knight activityUse", year: 2008 },
 ]
 
 export default function MaterialStepper({ type = "Add" }) {
-
   const classes = useStyles()
   const [activeStep, setActiveStep] = React.useState(0)
   const [skipped, setSkipped] = React.useState(new Set())
   const steps = getSteps()
 
-  const save = () => {  
-    saveData({ title, description });    
+  const save = () => {
+    saveData({
+      title,
+      description,
+      objective,
+      levelValue,
+      pupilTaskValue,
+      timePrep,
+      timeClass,
+      procBefore,
+      procIn,
+      book,
+      page,
+      followUp,
+      variations,
+      materials,
+      tips,
+      notes,
+      institute,
+      languageFocus,
+      activityUse,
+    })
   }
-  
-  //detail values 
-  const [title, setTitle] = React.useState("");
-  const [description, setDescription] = React.useState("");
-  const [levelValue, setLevelValue] = React.useState([{ title: 'The Shawshank Redemption', year: 1994 }]);
+
+  //detail values
+  const [title, setTitle] = React.useState("")
+  const [description, setDescription] = React.useState("")
+  const [levelValue, setLevelValue] = React.useState([])
+  const [pupilTaskValue, setPupilTaskValue] = React.useState([])
+  const [objective, setObjective] = React.useState("")
+  const [timePrep, setTimePrep] = React.useState([20, 40])
+  const [timeClass, setTimeClass] = React.useState([20, 40])
+  const [procBefore, setProcBefore] = React.useState("")
+  const [procIn, setProcIn] = React.useState("")
+  const [book, setBook] = React.useState("")
+  const [page, setPage] = React.useState("")
+  const [followUp, setFollowUp] = React.useState("")
+  const [variations, setVariations] = React.useState("")
+  const [materials, setMaterials] = React.useState("")
+  const [tips, setTips] = React.useState("")
+  const [notes, setNotes] = React.useState("")
+  const [instituteValue, setInstituteValue] = React.useState([])
+  const [activityUseValue, setActivityUseValue] = React.useState([])
+  const [languageFocusValue, setLanguageFocusValue] = React.useState([])
 
   const changeLevel = (e, value) => {
-    console.log("change level" , value);   
-    //
-    
-    if (!value[value.length -1].hasOwnProperty("title")) {
-      const lastValue = value.pop(value[value.length]);  
+    console.log("change level", value)
+    //Check if value passed is object with title i.e. from db or a new item
+    if (value && !value[value.length - 1].hasOwnProperty("title")) {
+      const lastValue = value.pop(value[value.length])
       const lastValueItem = { title: lastValue, year: 2011 }
-      value.push(lastValueItem);
-    } 
+      value.push(lastValueItem)
+    }
     setLevelValue(value)
-    
+  }
+
+  const changePupilTask = (e, value) => {
+    console.log("change pupil level", value)
+    //Check if value passed is object with title i.e. from db or a new item
+    if (value && !value[value.length - 1].hasOwnProperty("title")) {
+      const lastValue = value.pop(value[value.length])
+      const lastValueItem = { title: lastValue, year: 2011 }
+      value.push(lastValueItem)
+    }
+    setPupilTaskValue(value)
+  }
+
+  const changeInstitute = (e, value) => {
+    console.log("change Institute", value)
+    //Check if value passed is object with title i.e. from db or a new item
+    if (value && !value[value.length - 1].hasOwnProperty("title")) {
+      const lastValue = value.pop(value[value.length])
+      const lastValueItem = { title: lastValue, year: 2011 }
+      value.push(lastValueItem)
+    }
+    setInstituteValue(value)
+  }
+
+  const changeLanguageFocus = (e, value) => {
+    console.log("change Institute", value)
+    //Check if value passed is object with title i.e. from db or a new item
+    if (value && !value[value.length - 1].hasOwnProperty("title")) {
+      const lastValue = value.pop(value[value.length])
+      const lastValueItem = { title: lastValue, year: 2011 }
+      value.push(lastValueItem)
+    }
+    setLanguageFocusValue(value)
+  }
+
+  const changeActivityUse = (e, value) => {
+    console.log("change Institute", value)
+    //Check if value passed is object with title i.e. from db or a new item
+    if (value && !value[value.length - 1].hasOwnProperty("title")) {
+      const lastValue = value.pop(value[value.length])
+      const lastValueItem = { title: lastValue, year: 2011 }
+      value.push(lastValueItem)
+    }
+    setActivityUseValue(value)
   }
 
   function getStepContent(step) {
@@ -67,17 +171,57 @@ export default function MaterialStepper({ type = "Add" }) {
       case 0:
         return <MediaFiles />
       case 1:
-        return <MaterialDetails 
-                  title={title} 
-                  setTitle={setTitle} 
-                  description={description} 
-                  setDescription={setDescription} 
-                  levelValue={levelValue}
-                  setLevelValue={changeLevel}
-                  levels={levels}
-                  />
+        return (
+          <MaterialDetails
+            title={title}
+            setTitle={setTitle}
+            description={description}
+            setDescription={setDescription}
+            levelValue={levelValue}
+            setLevelValue={changeLevel}
+            levels={levels}
+            objective={objective}
+            setObjective={setObjective}
+            timePrep={timePrep}
+            setTimePrep={setTimePrep}
+            timeClass={timeClass}
+            setTimeClass={setTimeClass}
+            pupilTasks={pupilTasks}
+            pupilTaskValue={pupilTaskValue}
+            setPupilTaskValue={changePupilTask}
+          />
+        )
       case 2:
-        return "Complete material"
+        return (
+          <MaterialDetailsFull
+            procIn={procIn}
+            setProcIn={setProcIn}
+            book={book}
+            setBook={setBook}
+            page={setPage}
+            followUp={followUp}
+            setFollowUp={setFollowUp}
+            varitations={variations}
+            setVariations={setVariations}
+            procBefore={procBefore}
+            setProcBefore={setProcBefore}
+            materials={materials}
+            setMaterials={setMaterials}
+            tips={tips}
+            setTips={setTips}
+            notes={notes}
+            setNotes={setNotes}
+            institute={institute}
+            instituteValue={instituteValue}
+            setInstituteValue={changeInstitute}
+            languageFocus={languageFocus}
+            langFocusValue={languageFocusValue}
+            setLangFocusValue={changeLanguageFocus}
+            activityUse={activityUse}
+            activityUseValue={activityUseValue}
+            setActivityUse={changeActivityUse}
+          />
+        )
       default:
         return "Unknown step"
     }
@@ -124,7 +268,7 @@ export default function MaterialStepper({ type = "Add" }) {
   const handleReset = () => {
     setActiveStep(0)
   }
-    
+
   return (
     <div className={classes.root}>
       <h1>{type} Material</h1>
